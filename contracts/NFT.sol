86e25ba3cce7c268eeea84d7af3674885c8963a3
YoRHa <snowline2015@gmail.com>
2022-12-05 07:37:51 +0700

    deployed contract

---
+++ contracts/NFT.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "operator-filter-registry/src/DefaultOperatorFilterer.sol";
+
+contract NFT is ERC721, DefaultOperatorFilterer {
+    using Counters for Counters.Counter;
+    Counters.Counter private currentTokenId;
+    
+    constructor() ERC721("YoRHa", "YORHA") {}
+    
+    function mintTo(address recipient)
+        public
+        returns (uint256)
+    {
+        currentTokenId.increment();
+        uint256 newItemId = currentTokenId.current();
+        _safeMint(recipient, newItemId);
+        return newItemId;
+    }
+  
+    function setApprovalForAll(address operator, bool approved) public override onlyAllowedOperatorApproval(operator) {
+        super.setApprovalForAll(operator, approved);
+    }
+
+    function approve(address operator, uint256 tokenId) public override onlyAllowedOperatorApproval(operator) {
+        super.approve(operator, tokenId);
+    }
+
+    function transferFrom(address from, address to, uint256 tokenId) public override onlyAllowedOperator(from) {
+        super.transferFrom(from, to, tokenId);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId) public override onlyAllowedOperator(from) {
+        super.safeTransferFrom(from, to, tokenId);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data)
+        public
+        override
+        onlyAllowedOperator(from)
+    {
+        super.safeTransferFrom(from, to, tokenId, data);
+    }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,26 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const {
+  ALCHEMY_KEY,
+  METAMASK_KEY
+} = process.env;
+
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+      accounts: [`0x${METAMASK_KEY}`]
+    },
+    ethereum: {
+      chainId: 1,
+      url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+      accounts: [`0x${METAMASK_KEY}`]
+    },
+  },
 };
